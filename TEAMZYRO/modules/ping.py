9fa188a67c3a36dee965b90813c045ee041f1817
Shadow-Noxxx <jangraji518@gmail.com>
2025-06-06 12:02:29 +0530

    Update ping.py

--- TEAMZYRO/modules/ping.py
+++ TEAMZYRO/modules/ping.py
@@ -1,18 +1,17 @@
 import time
-
 from telegram import Update
-from telegram.ext import CommandHandler, CallbackContext
+from telegram.ext import CommandHandler, ContextTypes
 
 from TEAMZYRO import application, sudo_users
 
-async def ping(update: Update, context: CallbackContext) -> None:
-    if str(update.effective_user.id) not in sudo_users:
-        update.message.reply_text("Nouu.. its Sudo user's Command..")
+async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if update.effective_user.id not in sudo_users:
+        await update.message.reply_text("Nouu.. it's a Sudo user's command..")
         return
     start_time = time.time()
     message = await update.message.reply_text('Pong!')
     end_time = time.time()
     elapsed_time = round((end_time - start_time) * 1000, 3)
-    await message.edit_text(f'Pong! {elapsed_time}ms')
+    await message.edit_text(f'Pong! {elapsed_time} ms')
 
 application.add_handler(CommandHandler("ping", ping))
